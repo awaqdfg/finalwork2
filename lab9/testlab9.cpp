46b63d51ffd22e983e0f6f6a8bdc599f39bd4794
Nikita <nkitoz052760517@mail.ru>
2025-04-22 23:34:03 +0300

    add new file 2 student

--- lab9/testlab9.cpp
+++ lab9/testlab9.cpp
@@ -5,20 +5,24 @@
 #include <string.h>
 #include <locale.h>
 
-
 #define NUM_GAZETS 5
 
-struct Gazeta {
-    char name[50];
-    char data[20];
-    int kol; //количество газет
-    int cena; //цена газеты 
+struct Gazeta
+{
+    char name[50];     // Название газеты
+    char data[20];     // Дата выпуска
+    int kol;           // Количество экземпляров
+    int cena;          // Цена за один экземпляр
 };
 
-void inputGazetaData(struct Gazeta gazets[], int n) {
+
+void inputGazetaData(struct Gazeta gazets[], int n)
+{
+
     char name_data[70];
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         printf("Введите данные для газеты %d:\n", i + 1);
 
         printf("Название и дата выпуска: ");
@@ -30,44 +34,58 @@ void inputGazetaData(struct Gazeta gazets[], int n) {
 
         printf("Кол-во: ");
         scanf("%d", &gazets[i].kol);
+
         printf("Цена: ");
         scanf("%d", &gazets[i].cena);
-        
 
         getchar();
     }
 }
 
-void printGazetaData(struct Gazeta gazets[], int n) {
+void printGazetaData(struct Gazeta gazets[], int n)
+{
     printf("------------------------------------------------------------\n");
-    printf("| %-28s | %-10s | %-12s |\n","          Выпуски", " ", " " );
-    printf("| %-26s | %-10s | %-12s |\n","----------------------------", " ", " ");
+    printf("| %-28s | %-10s | %-12s |\n", "          Выпуски", " ", " ");
+    printf("| %-26s | %-10s | %-12s |\n", "----------------------------", " ", " ");
     printf("| %-15s | %-10s | %-10s | %-12s |\n", "Название", "Дата", "Кол-во", "Цена, шт.");
     printf("------------------------------------------------------------\n");
 
-    for (int i = 0; i < n; i++) {
-        printf("| %-15s | %-10s | %-10d | %-12d |\n", gazets[i].name, gazets[i].data, gazets[i].kol, gazets[i].cena);
+    for (int i = 0; i < n; i++)
+    {
+        printf("| %-15s | %-10s | %-10d | %-12d |\n",
+            gazets[i].name,
+            gazets[i].data,
+            gazets[i].kol,
+            gazets[i].cena);
         printf("------------------------------------------------------------\n");
     }
 }
 
-void swapFirstAndLast(struct Gazeta gazets[], int n) {
-    if (n > 1) {
+void swapFirstAndLast(struct Gazeta gazets[], int n)
+{
+    if (n > 1)
+    {
         struct Gazeta temp = gazets[0];
         gazets[0] = gazets[n - 1];
         gazets[n - 1] = temp;
     }
 }
 
+int main()
+{
+    system("chcp 1251"); 
+    system("cls");        
 
-int main() {
-    system("chcp 1251");
-    system("cls");
+
     struct Gazeta gazets[NUM_GAZETS];
+
     inputGazetaData(gazets, NUM_GAZETS);
+
     printf("Исходные данные:\n");
     printGazetaData(gazets, NUM_GAZETS);
+
     swapFirstAndLast(gazets, NUM_GAZETS);
+
     printf("Данные после перестановки первой и последней строки:\n");
     printGazetaData(gazets, NUM_GAZETS);
 
